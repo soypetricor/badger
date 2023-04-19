a8ddbc08cc92bcad2977cc1b9b3f2129e4235400
Yomarcy Sampayo <soypetricor@gmail.com>
2023-04-19 15:58:23 -0500

    formulario sin añadir imágen, guardar o cancelar

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -1,25 +1,103 @@
 import React, { useState } from 'react';
-import './InputStyle.css'
+import './Style.css'
 
+const initialForm ={
+  name: '',
+  specialty: '',
+  platform: '',
+  generation: '',
+  criteria: '',
+  knowledge: '',
+  skills: '',
+}
 function Form() {
-  const [valor, setValor] = useState('');
+  const [form, setForm] = useState(initialForm);
 
-  const handleChange = (event) => {
-    setValor(event.target.value);
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.id]:e.target.value,
+    })
+    console.log(form)
   }
 
   return (
-    <div>
-      <label htmlFor="mi-input"></label>
+    <>
+    <div align="center">
+    <h1 className='name-style'>Crea tu Badge</h1>
+    </div>
+    
+    <div align="center" className="container">
       <input 
-        id="mi-input"
+        id="name"
         type="text"
-        value={valor}
+        value={form.name}
         onChange={handleChange}
         placeholder='Nombre del Badge'
         className='input-style'
         />
     </div>
+    <div align="center" className="container">
+      <input 
+        id="specialty"
+        type="text"
+        value={form.specialty}
+        onChange={handleChange}
+        placeholder='Especialidad'
+        className='input-style'
+        />
+    </div>
+    <div align="center" className="container">
+      <input 
+        id="platform"
+        type="text"
+        value={form.platform}
+        onChange={handleChange}
+        placeholder='Plataforma/ evento'
+        className='input-style'
+        />
+    </div>
+    <div align="center" className="container">
+      <input 
+        id="generation"
+        type="text"
+        value={form.generation}
+        onChange={handleChange}
+        placeholder='Generación/ versión'
+        className='input-style'
+        />
+    </div>
+    <div align="center" className="container">
+      <input 
+        id="criteria"
+        type="text"
+        value={form.criteria}
+        onChange={handleChange}
+        placeholder='Criterios evaluativos'
+        className='input-style'
+        />
+    </div>
+    <div align="center" className="container">
+      <input 
+        id="knowledge"
+        type="text"
+        value={form.knowledge}
+        onChange={handleChange}
+        placeholder='Conocimientos'
+        className='input-style'
+        />
+    </div>
+    <div align="center" className="container">
+      <input 
+        id="skills"
+        type="text"
+        value={form.skills}
+        onChange={handleChange}
+        placeholder='Habilidades'
+        className='input-style'
+        />
+    </div>
+    </>
   );
   }
   
