91dc2749c76acd575bbb8979891df49949d8b31e
Yomarcy Sampayo <soypetricor@gmail.com>
2023-04-19 19:00:06 -0500

    estandarizando el código

--- src/components/CreateForm/CreateForm.js
+++ src/components/CreateForm/CreateForm.js
@@ -19,7 +19,7 @@ function Form() {
       ...form,
       [e.target.id]:e.target.value,
     })
-    console.log(form)
+    console.log(form, image)
   }
 
   const handleImageChange = (e) => {
@@ -38,88 +38,88 @@ function Form() {
 
   return (
     <>
-    <div align="center">
+    <div align='center'>
     <h1 className='name-style'>Crea tu Badge</h1>
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
     {image && (
-      <img src={image.url} alt="preview" className="image-preview" />
+      <img src={image.url} alt='preview' className='image-preview' />
     )}
-    <label htmlFor="image" className="custom-file-upload">
-    <i className="fa fa-cloud-upload"></i> Añadir imagen
+    <label htmlFor='image' className='custom-file-upload'>
+    <i className='fa fa-cloud-upload'></i> Añadir imagen
     </label>
     <input 
-      id="image"
-      type="file"
-      accept="image/*"
+      id='image'
+      type='file'
+      accept='image/*'
       onChange={handleImageChange}
       style={{ display: 'none' }}
     />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="name"
-        type="text"
+        id='name'
+        type='text'
         value={form.name}
         onChange={handleChange}
         placeholder='Nombre del Badge'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="specialty"
-        type="text"
+        id='specialty'
+        type='text'
         value={form.specialty}
         onChange={handleChange}
         placeholder='Especialidad'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="platform"
-        type="text"
+        id='platform'
+        type='text'
         value={form.platform}
         onChange={handleChange}
         placeholder='Plataforma/ evento'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="generation"
-        type="text"
+        id='generation'
+        type='text'
         value={form.generation}
         onChange={handleChange}
         placeholder='Generación/ versión'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="criteria"
-        type="text"
+        id='criteria'
+        type='text'
         value={form.criteria}
         onChange={handleChange}
         placeholder='Criterios evaluativos'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="knowledge"
-        type="text"
+        id='knowledge'
+        type='text'
         value={form.knowledge}
         onChange={handleChange}
         placeholder='Conocimientos'
         className='input-style'
         />
     </div>
-    <div align="center" className="container">
+    <div align='center' className='container'>
       <input 
-        id="skills"
-        type="text"
+        id='skills'
+        type='text'
         value={form.skills}
         onChange={handleChange}
         placeholder='Habilidades'
