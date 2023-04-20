9b3505b0a510a12c7fdbe9a5510100b9f529a957
Yomarcy Sampayo <soypetricor@gmail.com>
2023-04-20 16:27:26 -0500

    conectando SignBadge page

--- src/pages/SignBadge/index.js
+++ src/pages/SignBadge/index.js
@@ -1,7 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router';
+import './Style.css'
+
 function App() {
+    let navigate = useNavigate()
     return (
-      <div className="App">
-          Estamos trabajando para que pronto puedas firmar tus badges
+      <div className="App" align="center">
+         <p className='text'> Estamos trabajando para que pronto puedas firmar tus badges</p>
+         <button className='button' onClick={() => navigate('/')}>Vuelve al inicio</button>
       </div>
     );
   }
